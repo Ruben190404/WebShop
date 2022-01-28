7ad5b8b5f45c97eedc253b8c0bae3b739fec0081
rluteijn0253 <92361480+rluteijn0253@users.noreply.github.com>
2022-01-28 08:48:17 +0100

    Update Product.php

--- Product.php
+++ Product.php
@@ -22,4 +22,17 @@ class Product
 
         return $stmt->fetchAll();
     }
+    public function addProduct($name, $price, $category, $description, $targetFile)
+    {
+        $db = new Database();
+        $conn = $db->connect();
+
+        $statement = $conn->prepare("insert into products (name, price, category, description, image) values (:name, :price, :category, :description, :image)");
+        $statement->bindParam(":name", $name);
+        $statement->bindParam(":price", $price);
+        $statement->bindParam(":description", $description);
+        $statement->bindParam(":category", $category);
+        $statement->bindParam(":image", $targetFile);
+        $statement->execute();
+    }
 }
