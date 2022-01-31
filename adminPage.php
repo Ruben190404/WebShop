134032a61f433a9345401b05d8237d91b5ffc813
rluteijn0253 <92361480+rluteijn0253@users.noreply.github.com>
2022-01-31 08:41:23 +0100

    Add files via upload

--- adminPage.php
+++ adminPage.php
@@ -1,5 +1,5 @@
 <?php
-include 'adminProduct.php';
+include 'Product.php';
 
 $servername = "127.0.0.1";
 $username = "root";
@@ -21,7 +21,7 @@ if (isset($_POST['productId'])) {
     $stmt->execute();
 }
 
-$newProduct = new adminProduct();
+$newProduct = new Product();
 
 if (!empty($_POST) && !isset($_POST['productId'])) {
     $image_file= $_FILES["image"]["name"];
@@ -43,9 +43,41 @@ if (!empty($_POST) && !isset($_POST['productId'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
-    <img class="ms-2" src="images/tiger-removebg-preview.png" width="auto" height="75px">
-    <div class="ms-3">Admin Page</div>
-    <div class="container h-50 overflow-auto pt-3">
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/tiger.svg" alt="" width="60" height="auto">
+            </a>
+            <a href="index.php" class="navbar-brand text-warning">Wok & Roll</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navmenu">
+                <ul class="navbar-nav ms-auto">
+                    <li id="google_translate_element"></li>
+                    <script type="text/javascript">
+                        function googleTranslateElementInit() {
+                            new google.translate.TranslateElement({pageLanguage: 'nl'}, 'google_translate_element');
+                        }
+                    </script>
+                    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                    <li class="nav-item">
+                        <a href="workshop.html" class="nav-link">Workshop's</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.html" class="nav-link">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login.php" class="nav-link">login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="mt-5">Admin Page</div>
+    <div class="container h-50 overflow-auto pt-4">
         <table  class="table table-hover table-striped">
             <thead>
             <tr>
